06f64664ae78a707285485f654962cdaf68fd5a7
Dein Name <deine@email.de>
2025-11-18 15:41:33 +0100

    Remove isActive from workshop creation

--- app/api/auth/register/workshop/route.ts
+++ app/api/auth/register/workshop/route.ts
@@ -61,7 +61,6 @@ export async function POST(request: Request) {
             description: validatedData.description,
             sepaMandateRef: sepaMandateRef,
             isVerified: false, // Admin muss verifizieren
-            isActive: true,
           }
         }
       },

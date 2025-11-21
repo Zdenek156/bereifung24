1b2a23492ba40169f3d59e3b0058b946ec8ae5d7
Dein Name <deine@email.de>
2025-11-21 15:31:55 +0100

    Fix Next.js Server Actions with reverse proxy

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
       },
     ],
   },
+  // Fix for HTTPS behind reverse proxy
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['reifen.bereifung24.de', 'localhost:3000'],
+    },
+  },
 }
 
 module.exports = nextConfig

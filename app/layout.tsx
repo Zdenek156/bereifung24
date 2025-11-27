46a2d543ca2243b90181ed42a42271cd9c321848
Dein Name <deine@email.de>
2025-11-27 15:46:09 +0100

    Add global cookie banner component that appears on all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import SessionWrapper from './components/SessionWrapper'
+import CookieBanner from '@/components/CookieBanner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <SessionWrapper>
           {children}
+          <CookieBanner />
         </SessionWrapper>
       </body>
     </html>

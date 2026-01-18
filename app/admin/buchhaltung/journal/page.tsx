67e7e9cf6da22141f8b40fbc46ec7a2584b66597
Dein Name <deine@email.de>
2026-01-18 18:52:54 +0100

    Fix: Link-Import + B24EMPLOYEE Berechtigungen für alle Buchhaltungsseiten

--- app/admin/buchhaltung/journal/page.tsx
+++ app/admin/buchhaltung/journal/page.tsx
@@ -2,6 +2,7 @@
 
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import BackButton from '@/components/BackButton'
 
